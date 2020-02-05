0931dc16caf32bffa04eb0aadbaff6dba4817a5a
SwethaChintala <60467177+SwethaChintala@users.noreply.github.com>
2020-02-05 15:35:35 +0530

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,3 +1,14 @@
-a = 1
-a++
-puts a
+#Write a program to demonstrate difference between While and Until.
+
+$a=10
+$b=4
+while $a>$b
+	puts "#$a"
+	$a-=1
+end
+$c=50
+$d=40
+until $c<$d
+	puts "#$c"
+	$c-=1
+end
