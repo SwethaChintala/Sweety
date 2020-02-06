8aaa326ac253a1884f274f66760cc9c8302a650d
SwethaChintala <60467177+SwethaChintala@users.noreply.github.com>
2020-02-06 12:58:42 +0530

    Create typoci.rb

---
+++ typoci.rb
@@ -0,0 +1,17 @@
+class Aclara
+	@@place='India'
+	def initialize(teamname,projectname)
+		@team=teamname
+		@project=projectname
+	end
+	def display()
+		puts "team name is #@team"
+		puts "project name is #@project"
+		puts "team size is #@@place"
+		puts "company name is #$companyname"
+	enda
+end
+det1 = Aclara.new("vega","Aclara_one")
+det2 = Aclara.new("avengers","Aclara_one")
+det1.disply()
+det2.display()
