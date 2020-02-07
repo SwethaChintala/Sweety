b5154b921bc238d9fb3cc1e4f2dcaed1e4597e0a
SwethaChintala <60467177+SwethaChintala@users.noreply.github.com>
2020-02-07 12:19:18 +0530

    Create duplicate.rb

---
+++ duplicate.rb
@@ -0,0 +1,68 @@
+x=0
+y=1
+begin
+	puts "Welcome"
+	x+=1
+
+while x==y
+y+=1
+while x==y do
+	puts "Aclara"
+	x+=1
+loop do
+	puts "Hello"
+	x+=5
+	puts x
+	break
+end
+x=0
+y=1
+begin
+	puts "Welcome"
+	x+=1
+
+while x==y
+y+=1
+while x==y do
+	puts "Aclara"
+	x+=1
+loop do
+	puts "Hello"
+	x+=5
+	puts x
+	break
+end
+x=0
+y=1
+begin
+	puts "Welcome"
+	x+=1
+
+while x==y
+y+=1
+while x==y do
+	puts "Aclara"
+	x+=1
+loop do
+	puts "Hello"
+	x+=5
+	puts x
+	break
+end
+x=0
+y=1
+begin
+	puts "Welcome"
+	x+=1
+
+while x==y
+y+=1
+while x==y do
+	puts "Aclara"
+	x+=1
+loop do
+	puts "Hello"
+	x+=5
+	puts x
+	break
+end
